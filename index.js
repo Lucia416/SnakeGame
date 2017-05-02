1f16457b0cf047f638a1636ccc290937a2f8ecf0
Lucia416 <lucia@lucia3d.com>
2017-05-01 20:09:42 -0400

    snake moving

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 var headY = 2;
 var headX = 2;
-var height= 30;
+var height = 30;
 var width = 30;
-var update = 100;
+var interval = 100;
 var increment = 2;
 
 var tailY = [headY];
 var tailX = [headX];
 var foodY;
 var foodX;
-var moving;
-var gameOver;
+var moving = false;
+var gameOver = false;
 var dir = 0 // down = 0, up = 1, left = 2, right = 3
 var int;
 
@@ -32,7 +32,7 @@ const map = () => {
   for(var y = 0; y < height; y++){
     document.write('<tr>');
     for( var x = 0; x < width; x++){
-      if( x === 0 || x === width - 1 || y === 0 || y === height -1){
+      if( x === 0 || x === width - 1 || y === 0 || y === height - 1){
         document.write("<td class='borders' id= '"+ x + "-" + y + "'></td>");
       }else{
         document.write("<td class='arena' id= '"+ x + "-" + y + "'></td>");
@@ -42,6 +42,91 @@ const map = () => {
 
   }
   document.write('</table>');
+}
+
+const snake = () => {
+  set(headX, headY, "snake");
+}
+
+const get = (x,y) => {
+  return document.getElementById(x+"-"+y);
+}
+
+const set = (x,y,value) => {
+  if(x!= null && y != null);
+  get(x,y).setAttribute("class", value);
+
+}
+
+const makeType = (x,y) => {
+  return get(x,y).getAttribute("class");
+}
+
+const food = () => {
+  var found = false;
+  while(!found && (length < (width -2)* (height-2)+1)){
+    var foodX = rand(1, width-1);
+    var foodY = rand(1, height-1);
+    if(makeType(foodY,foodX) === "arena")
+    found = true;
+  }
+  set(foodX,foodY, "food");
+  fX= foodX;
+  fY= foodY;
+}
+
+const rand = (min,max) => {
+    return Math.floor(Math.random()*(max-min)+min);
+}
+
+window.addEventListener("keypress", function key(){
+  var key = event.keyCode;
+  if(dir != 0 && (key === 119 || key === 87))
+    dir = 1;
+  else if(dir != 1 && (key === 115 || key === 83))
+    dir = 0;
+  else if(dir != 3 && (key === 97 || key === 65))
+    dir = 2;
+  else if( dir != 2 && (key === 100 || key === 68))
+    dir = 3;
+
+  if(!moving)
+    moving = true;
+  else if(key === 32)
+    moving = false;
+})
+
+const update = () => {
+  set( fX, fY, "food");
+  set(tailX[length], tailY[length], "arena");
+  if (dir === 1)
+    headY--;
+  else if(dir === 0)
+    headY++;
+  else if(dir === 2)
+    headX--;
+  else if (dir === 3)
+    headX++;
+  set(headX, headY, "snake");
+}
+
+const updTail = () => {
+  for(var i = length; i > 0; i--){
+    tailX[i] = tailX[i-1];
+    tailY[i] = tailY[i-1];
+  }
+  tailX[0] = headX;
+  tailY[0] = headY;
+
+}
+
+const loop = () => {
+  if (moving && !gameOver){
+    update();
+  }else if(gameOver){
+    clearInterval(int);
+  }
+
 }
 
 move();
